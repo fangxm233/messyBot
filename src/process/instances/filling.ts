6d202b0cb08fc695738dc70ee19743f1e5a4c224
fangxm <1126716733@QQ.COM>
2020-05-31 10:15:28 +0800

    添加空检查

--- src/process/instances/filling.ts
+++ src/process/instances/filling.ts
@@ -6,8 +6,8 @@ import { RoleFactory } from "../../roles/roleFactory";
 import { RolePC } from "../../roles/powerCreep";
 
 @profile
-export class ProcessFilling extends Process{
-    constructor(roomName: string){
+export class ProcessFilling extends Process {
+    constructor(roomName: string) {
         super(roomName, 'filling');
     }
 
@@ -15,44 +15,48 @@ export class ProcessFilling extends Process{
         return new ProcessFilling(roomName);
     }
 
-    check(): boolean{
+    check(): boolean {
+        if (!Game.rooms[this.roomName]) {
+            this.close();
+            return false;
+        }
         this.foreachCreep(this.refreshAllot); //优化点
-        if(!Game.rooms[this.roomName].isFull) {
+        if (!Game.rooms[this.roomName].isFull) {
             return !RolePC.pcAbility[this.roomName]
         }
         return false;
     }
 
-    run(){
+    run() {
         this.foreachCreep(this.runCreep);
         let sleep = true;
-        this.foreachCreep(creep => { if(!creep.memory.sleep) sleep = false; });
-        if(sleep && Game.rooms[this.roomName].isFull) this.suspend();
+        this.foreachCreep(creep => { if (!creep.memory.sleep) sleep = false; });
+        if (sleep && Game.rooms[this.roomName].isFull) this.suspend();
     }
 
-    runCreep(creep : Creep){
+    runCreep(creep: Creep) {
         let role = RoleFactory.getRole(creep);
-        if(role) role.run();
+        if (role) role.run();
     }
 
-    refreshAllot(creep: Creep){
-        if(creep.ticksToLive && creep.ticksToLive <= creep.body.length * 3) return;
-        if(creep.memory.allotUnit) Alloter.refreshDirty(creep.memory.allotUnit);
+    refreshAllot(creep: Creep) {
+        if (creep.ticksToLive && creep.ticksToLive <= creep.body.length * 3) return;
+        if (creep.memory.allotUnit) Alloter.refreshDirty(creep.memory.allotUnit);
     }
 
     refillNeeded(spawnBusy: boolean) {
-        if(this.state == 'active') return;
-        if(!RolePC.pcAbility[this.roomName]) {
+        if (this.state == 'active') return;
+        if (!RolePC.pcAbility[this.roomName]) {
             this.awake();
             return;
         }
         let pc = RolePC.pcAbility[this.roomName][PWR_OPERATE_EXTENSION];
-        if(pc.cooldown) {
+        if (pc.cooldown) {
             this.awake();
             return;
         }
         let role = pc.role;
         role.refillNeeded = true;
-        if(!spawnBusy) this.awake();
+        if (!spawnBusy) this.awake();
     }
 }
